5945a6c17efaafa526d51bf4b378ad91e689d191
Daniel Falster <daniel.falster@unsw.edu.au>
2025-02-17 15:20:53 +1100

    Refine clean_zip.R

--- clean_zip.R
+++ clean_zip.R
@@ -2,13 +2,19 @@
 
 # This file makes a clean zip of a folder, removing
 # any special mac files and .Rproj.user
-# Example, /create_public.R my_folder
+# It will create a folder with the same name as the lesson material
+# E.g. ../../create_public.R Wk1-1-intro
 
 path <- commandArgs(TRUE)[1]
 
+original_wd <- getwd()          # Store current working directory
+on.exit(setwd(original_wd))  
+
+setwd(path) # Change to new working directory
+
 # Remove files that don't want in public release
 files <- 
-  list.files(path, full.names = TRUE, recursive = TRUE, all.files = TRUE)
+  list.files(".", full.names = TRUE, recursive = TRUE, all.files = TRUE)
 
 unwanted <- c("TODO", ".DS_", "Rproj.user", "WIP/", "X_setup", "solutions", "ignore", ".Rhistory", ".git", "output", ".key")
 
@@ -19,4 +25,7 @@ for(f in unwanted)
 # X excludes specieal mac files
 # 9 max compression
 # r recursive
-utils::zip(paste0(path, ".zip"), files,  flags = "-9X")
+filename <- paste0(original_wd, "/", basename(path), ".zip")
+
+message(sprintf("Creating file %s\n", filename))
+utils::zip(filename, files,  flags = "-9X")
