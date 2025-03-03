179810a3c5bae0bc2cc23732cff3f48577c528f0
Fonti Kar <f.kar@unsw.edu.au>
2025-03-03 11:48:14 +1100

    Digitised raw data

--- week 3/Wk3-1-moodle/metadigitise/jasmine-digitise.R
+++ week 3/Wk3-1-moodle/metadigitise/jasmine-digitise.R
@@ -4,12 +4,26 @@ library(dplyr)
 
 browseVignettes("metaDigitise")
 
-data <- metaDigitise(dir = "week 3/Wk3-1-moodle/metadigitise/")
+# Choose 2. Import then 1. All data to read into R
+data <- metaDigitise(dir = "week 3/Wk3-1-moodle/metadigitise/", summary = FALSE) 
 
-cleaned_data <- data |> 
-  mutate(group_id = case_when(group_id == "20" ~ "20C",
-                              TRUE ~ group_id)) |> 
-  arrange(variable) |> 
+fig3_data <- data$scatterplot$fig3.png
+fig4_data <- data$scatterplot$fig4.png
+
+fig3_data |> count(group) 
+fig4_data |> count(group)  # Use fig 4, the sample sizes match the paper. 
+
+clean_data <- fig4_data |> 
+  mutate(temp_treatment = case_when(group == 1 ~ "20C",
+                                    group == 2 ~ "24C",
+                                    group == 3 ~ "28C")
+         ) |> 
+  group_by(temp_treatment) |> 
+  mutate(fish_id = paste("fish_", row_number())
+         ) |> 
+  rename(smr = x) |> 
+  select(fish_id, temp_treatment, smr) |> 
   tibble()
 
-write_csv(cleaned_data, "week 3/Wk3-1-moodle/metadigitise/data/jasmine-mr.csv")
+write_csv(clean_data, "week 3/Wk3-1-moodle/metadigitise/data/jasmine-fish-smr.csv")
+write_csv(clean_data, "week 3/Wk3-1-moodle/data/jasmine-fish-smr.csv")
