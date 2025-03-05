6d9fb2c965ab477ef45b604defc1e377063f2784
Daniel Falster <daniel.falster@unsw.edu.au>
2025-03-06 10:05:48 +1100

    Enable webr creation in rendering

--- R/generate_moodle_quiz.R
+++ R/generate_moodle_quiz.R
@@ -1,4 +1,4 @@
-generate_moodle_quiz <- function(file) {
+generate_moodle_quiz <- function(file, webr = FALSE) {
 
   file_moodle_quiz <- str_replace(file, ".qmd", "-moodle.rmd")
   yaml <- rmarkdown::yaml_front_matter(file)
@@ -18,7 +18,7 @@ editor_options:
 
 ```{r,  echo=FALSE, message=FALSE, warning=FALSE}
 # webr counter
-# source('R/webr-counter.R')
+source('../../R/webr-counter.R')
 library(moodlequiz)
 ```\n\n", yaml$title, yaml$title
 )
@@ -34,6 +34,11 @@ library(moodlequiz)
     lines[-seq(1, i)] |>
     remove_solutions() |>
     add_headers() 
+  
+  if(webr) {
+    new_lines <- new_lines |>
+      add_webr()
+  }
 
   c(front_matter, new_lines) |>
   writeLines(file_moodle_quiz)
@@ -48,5 +53,15 @@ add_headers <- function(lines) {
     stringr::str_c(collapse = "NNN") |>
     # the regular expression searches for everything between  NNNN##  and NNNN, dpluicates the lines with h3 tags and then splits the string back into lines
     stringr::str_replace_all("NNN## (.+?)(?=NNN)", "NNN## \\1NNNNNN<h3>\\1</h3>NNNNNN") |>
+    stringr::str_split_1("NNN")
+}
+
+
+add_webr <- function(lines) {
+  lines |>
+    # Using `NNNN` for new line when collapsing because this doesn't interfere with string replacement whereas `\n` does
+    stringr::str_c(collapse = "NNN") |>
+    # the regular expression searches for everything between  NNNN##  and NNNN, dpluicates the lines with h3 tags and then splits the string back into lines
+    stringr::str_replace_all("\`\`\`\\{r(.*?)\\}(.*?)\`\`\`", "<div id=`r class_id()`><pre>\\2</pre></div>") |>
     stringr::str_split_1("NNN")
 }
